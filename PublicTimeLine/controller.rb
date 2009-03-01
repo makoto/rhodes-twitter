898a5acb4ee6b1e5fc8d60b191f049e3af49aa98
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-03-01 11:13:30 +0000

    More change fro 0.2 to 0.3

--- PublicTimeLine/controller.rb
+++ PublicTimeLine/controller.rb
@@ -6,44 +6,45 @@ class PublicTimeLineController < Rho::RhoController
   #GET /PublicTimeLine
   def index
     @PublicTimeLines = PublicTimeLine.find(:all)
-    render :index
+    render :action => :index
   end
 
   # GET /PublicTimeLine/1
   def show
     @PublicTimeLines = PublicTimeLine.find(@params['object'])
+    render :action => :show
   end
 
   # GET /PublicTimeLine/new
   def new
     @PublicTimeLine = PublicTimeLine.new
-    render :new
+    render :action => :new
   end
 
   # GET /PublicTimeLine/1/edit
   def edit
     @PublicTimeLine = PublicTimeLine.find(@params['id'])
-    render :edit
+    render :action => :edit
   end
 
   # POST /PublicTimeLine/create
   def create
     @PublicTimeLine = PublicTimeLine.new(@params['PublicTimeLine'])
     @PublicTimeLine.save
-    redirect :index
+    redirect :action => :index
   end
 
   # POST /PublicTimeLine/1/update
   def update
     @PublicTimeLine = PublicTimeLine.find(@params['id'])
     @PublicTimeLine.update_attributes(@params['PublicTimeLine'])
-    redirect :index
+    redirect :action => :index
   end
 
   # POST /PublicTimeLine/1/delete
   def delete
     @PublicTimeLine = PublicTimeLine.find(@params['id'])
     @PublicTimeLine.destroy
-    redirect :index
+    redirect :action => :index
   end
 end
