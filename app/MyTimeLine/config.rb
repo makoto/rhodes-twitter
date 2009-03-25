c600bc1c04e1147f0d5f0aa425a84bbd0f8f4c37
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-03-25 08:34:54 +0000

    Disabled sync for MyTimeLine and PublicTimeLine

--- app/MyTimeLine/config.rb
+++ app/MyTimeLine/config.rb
@@ -1,3 +1,4 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("MyTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/12", "source_id"=>12})
+# Rho::RhoConfig::add_source("MyTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/12", "source_id"=>12})
+Rho::RhoConfig::add_source("MyTimeLine", {"url"=>"", "source_id"=>12})
