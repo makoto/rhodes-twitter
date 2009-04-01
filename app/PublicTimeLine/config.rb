0d1a07589c0bc953280694ab2752a2484fdbd115
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-04-02 00:42:57 +0100

    Re-enable PublicTime

--- app/PublicTimeLine/config.rb
+++ app/PublicTimeLine/config.rb
@@ -1,4 +1,4 @@
 require 'rho'
 
 # Rho::RhoConfig::add_source("PublicTimeLine", {"url"=>"", "source_id"=>11})
-# Rho::RhoConfig::add_source("PublicTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/11", "source_id"=>11})
+Rho::RhoConfig::add_source("PublicTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/11", "source_id"=>11})
