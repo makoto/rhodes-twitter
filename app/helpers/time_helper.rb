ec4884b9626141f385950caee325119c1058a5ac
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-04-05 16:44:52 +0100

    Added parse_time to display date in difference

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -7,14 +7,29 @@ class Rho::RhoController
   
   def parse_time(time_in_string)
     time_in_string = time_in_string.first if time_in_string.kind_of?(Array)
-    day_of_week, month, day, time, timee_zone, year = time_in_string.split
-    Time.mktime(year, month, day, time)
+
+    # This is due to annoying inconsistance between Twitter's search API and REST API
+    # http://twitter.com/makoto_inoue/status/1457257044
+    # rss or atom format returns consistent date format, but REST API rss/atom format
+    # does not include profile image url, while search api are.
+    if time_in_string.match(',')
+      # Search API date format: "time_in_string: Sun, 05 Apr 2009 14:37:34 +0000"
+      day_of_week, day, month, year, time, timee_zone = time_in_string.split
+    else
+      # REST API date format "time_in_string: Sun Apr 05 14:52:39 +0000 2009"
+      day_of_week, month, day, time, timee_zone, year = time_in_string.split
+    end
+
+    time_object = Time.mktime(year, month, day, time)
+    timeago(time_object)
   end
 
+private
   # From http://perezj.blogspot.com/2008/05/ruby-display-datetime-in-words-timeago.html
   def timeago(time, options = {})
     start_date = options.delete(:start_date) || Time.new
     date_format = options.delete(:date_format) || :default
+    p " (#{start_date.inspect}.to_i - #{time.inspect}.to_i).floor"
     delta_minutes = (start_date.to_i - time.to_i).floor / 60
     if delta_minutes.abs <= (8724*60)       
       distance = distance_of_time_in_words(delta_minutes)       
@@ -33,12 +48,12 @@ class Rho::RhoController
         "less than a minute"
       when minutes == 1
         "about 1 minute"
-      when minutes < 50
+      when  minutes < 50
         "about #{minutes} minutes"
       when minutes < 90
         "about one hour"
       when minutes < 1080
-        "#{(minutes / 60).round} hours"
+        "#{(minutes / 60.0).round} hours"
       when minutes < 1440
         "one day"
       when minutes < 2880
