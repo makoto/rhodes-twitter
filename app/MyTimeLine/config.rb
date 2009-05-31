9258f59050efabca77568544d307caebd4204089
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-05-31 14:33:55 +0100

    Changed source adapter to use rhohub

--- app/MyTimeLine/config.rb
+++ app/MyTimeLine/config.rb
@@ -1,3 +1,4 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("MyTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/12", "source_id"=>12})
+# Rho::RhoConfig::add_source("MyTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/12", "source_id"=>12})
+Rho::RhoConfig::add_source("MyTimeLine", {"url"=>"http://rmt.rhohub.com/apps/28/sources/155", "source_id"=>155})
--- app/PublicTimeLine/config.rb
+++ app/PublicTimeLine/config.rb
@@ -1,3 +1,4 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("PublicTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/11", "source_id"=>11})
+# Rho::RhoConfig::add_source("PublicTimeLine", {"url"=>"http://localhost:3000/apps/6/sources/11", "source_id"=>11})
+Rho::RhoConfig::add_source("PublicTimeLine", {"url"=>"http://rmt.rhohub.com/apps/28/sources/153", "source_id"=>153})
